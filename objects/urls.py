8b623e57fe34e856ba8369576d52a770dcd6c10f
Quvonchbek Murodov <quvonchbe05@gmail.com>
2023-09-11 13:20:02 +0500

    added objects

--- objects/urls.py
+++ objects/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
+from .views import ObjectList, ObjectCreate, ObjectEdit, ObjectDelete, ObjectDetail
+
 
 urlpatterns = [
-    
+    path('list', ObjectList.as_view()),
+    path('create', ObjectCreate.as_view()),
+    path('edit/<int:pk>', ObjectEdit.as_view()),
+    path('delete/<int:pk>', ObjectDelete.as_view()),
+    path('view/<int:pk>', ObjectDetail.as_view()),
 ]
--- objects/views.py
+++ objects/views.py
@@ -1,3 +1,53 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Object
+from .serializers import ObjectSerializer, ObjectListSerializer
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 
 # Create your views here.
+class ObjectList(generics.ListAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    queryset = Object.objects.all()
+    serializer_class = ObjectListSerializer
+    
+    
+    
+    
+    
+class ObjectCreate(generics.CreateAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    queryset = Object.objects.all()
+    serializer_class = ObjectSerializer
+    
+    
+    
+    
+    
+class ObjectEdit(generics.UpdateAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    queryset = Object.objects.all()
+    serializer_class = ObjectSerializer
+    
+    
+    
+    
+    
+class ObjectDelete(generics.DestroyAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    queryset = Object.objects.all()
+    serializer_class = ObjectSerializer
+    
+    
+    
+    
+    
+class ObjectDetail(generics.RetrieveAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    queryset = Object.objects.all()
+    serializer_class = ObjectListSerializer
