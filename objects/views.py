bfeb0234206a73b4c36445f3e508010003a16fbd
Quvonchbek Murodov <quvonchbe05@gmail.com>
2023-09-18 17:23:58 +0500

    add

--- objects/views.py
+++ objects/views.py
@@ -4,6 +4,9 @@ from .models import Object
 from .serializers import ObjectSerializer, ObjectListSerializer
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework.views import APIView
+from bid.models import Bid, BidProduct
+from rest_framework.response import Response
 
 # Create your views here.
 class ObjectList(generics.ListAPIView):
@@ -46,8 +49,45 @@ class ObjectDelete(generics.DestroyAPIView):
     
     
     
-class ObjectDetail(generics.RetrieveAPIView):
-    authentication_classes = [JWTAuthentication]
-    permission_classes = [IsAuthenticated]
-    queryset = Object.objects.all()
-    serializer_class = ObjectListSerializer
+class ObjectDetail(APIView):
+    # authentication_classes = [JWTAuthentication]
+    # permission_classes = [IsAuthenticated]
+    def get(self, request, pk):
+        object = Object.objects.filter(pk=pk).first()
+        if not object:
+            return Response(status=404, data={'error': 'Obyekt topilmadi'})
+        
+        obj_json = {
+            'id': object.pk,
+            'name': object.name,
+            'address': object.address,
+            'worker': {
+                'id': object.worker.pk,
+                'name': object.worker.name,
+                'phone': object.worker.phone,
+            },
+            'bids': []
+        }
+        
+        bid = Bid.objects.filter(object__pk=object.pk)
+        bid_arr = []
+
+        for b in bid:
+            obj_json['bids'].append(
+                {
+                    "id": b.pk,
+                    "status": b.status,
+                    "description": b.description,
+                    "created_at": b.created_at,
+                    "products": [],
+                }
+            )
+
+        for b in obj_json['bids']:
+            products = BidProduct.objects.filter(bid__pk=b["id"])
+            for p in products:
+                b["products"].append(
+                    {"id": p.pk, "name": p.product.name, "amount": p.amount, 'size': p.product.size}
+                )
+                
+        return Response(status=200, data=obj_json)
