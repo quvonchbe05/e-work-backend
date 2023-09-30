24fa19678d3fcfe50a5c776c1af62238fff1cdab
javokhir <inboxjavohir@gmail.com>
2023-09-30 19:09:28 +0500

    added cors package

--- config/settings.py
+++ config/settings.py
@@ -218,3 +218,7 @@ SIMPLE_JWT = {
     "SLIDING_TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainSlidingSerializer",
     "SLIDING_TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSlidingSerializer",
 }
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8000',
+]
