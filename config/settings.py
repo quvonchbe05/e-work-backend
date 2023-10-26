0215ddc3de57678759cab315fa199bb03d4006a9
javokhir <inboxjavohir@gmail.com>
2023-10-26 12:45:24 +0500

    add cors all origins true

--- config/settings.py
+++ config/settings.py
@@ -200,6 +200,7 @@ SIMPLE_JWT = {
     "SLIDING_TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSlidingSerializer",
 }
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:8000',
-]
+CORS_ALLOW_ALL_ORIGINS = True
+
+
+
